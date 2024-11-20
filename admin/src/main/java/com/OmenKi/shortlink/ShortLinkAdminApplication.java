7a2a07024e00e4ce6ffdc729591dafeb544a12fe
朱兆麒 <masin2001zzq@163.com>
2024-11-20 16:24:02 +0800

    feat: 创建短链接子模块项目

---
+++ admin/src/main/java/com/OmenKi/shortlink/ShortLinkAdminApplication.java
@@ -0,0 +1,16 @@
+package com.OmenKi.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Masin_Zhu
+ * @Date: 2024/11/14
+ * @Description:
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}

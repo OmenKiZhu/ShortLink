728593ea167e2b3b1f7ab076d8756a1835ed8c4e
朱兆麒 <masin2001zzq@163.com>
2024-12-16 16:49:17 +0800

    feat: 新增数据库持久层对象基础属性

---
+++ admin/src/main/java/com/OmenKi/shortlink/admin/common/database/BaseDO.java
@@ -0,0 +1,34 @@
+package com.OmenKi.shortlink.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: Masin_Zhu
+ * @Date: 2024/12/16
+ * @Description: 数据库持久层对象基础属性
+ */
+@Data
+
+public class BaseDO {
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private int delFlag;
+}

fb41293b643ddb5e25622673fee5f1b88991d1bd
朱兆麒 <masin2001zzq@163.com>
2025-01-08 17:18:43 +0800

    feature: 前端传递有效期时间序列化

--- project/src/main/java/com/OmenKi/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/OmenKi/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
@@ -1,6 +1,7 @@
 package com.OmenKi.shortlink.project.dto.req;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -44,7 +45,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
-    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**

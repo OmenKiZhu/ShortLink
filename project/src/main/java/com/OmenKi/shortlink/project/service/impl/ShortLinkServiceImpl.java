79cce7394aa9b44f0ac21bc462d04026bc179413
朱兆麒 <masin2001zzq@163.com>
2025-02-13 09:10:37 +0800

    fix: 修复永久有效短链接短链接从回收站恢复存在的validate空指针问题

--- project/src/main/java/com/OmenKi/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/OmenKi/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -302,7 +302,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .eq(ShortLinkDO::getDelFlag, 0)
                     .eq(ShortLinkDO::getEnableStatus, 0);
             ShortLinkDO shortLinkDO = baseMapper.selectOne(queryWrapper);
-            if(shortLinkDO == null || shortLinkDO.getValidDate().before(new Date())) {
+            if(shortLinkDO == null || (shortLinkDO.getValidDate() != null && shortLinkDO.getValidDate().before(new Date()))) {
                 //当作没短链接处理 设置个缓存空值，避免缓存穿透
                 stringRedisTemplate.opsForValue().set(String.format(GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl), "-", 30, TimeUnit.MINUTES);
                 ((HttpServletResponse) response).sendRedirect("/page/notfound");
